b1d9dc2f66130839014dbcfe12fedee231c7bf80
fco9 <133737127+fco1000@users.noreply.github.com>
2024-12-06 10:11:05 +0000

    added some packages and made some changes in the front and backend

---
+++ apps/frontend/CTC/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import books from "./assets/images/books.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={books} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>CTC</h1>
+    </>
+  );
+}
+
+export default App;
